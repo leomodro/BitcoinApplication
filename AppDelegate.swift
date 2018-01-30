483c4988389d099f7275d3405fc4cc2201f836b3
Leonardo Modro <leonardomodro@gmail.com>
2018-01-30 17:45:35 -0200

    Initial commit

---
+++ AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  BitcoinApplication
+//
+//  Created by Leonardo Modro on 30/01/2018.
+//  Copyright © 2018 Leonardo Modro. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let rootVC = ViewController()
+        window?.rootViewController = rootVC
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) { }
+    func applicationDidEnterBackground(_ application: UIApplication) { }
+    func applicationWillEnterForeground(_ application: UIApplication) { }
+    func applicationDidBecomeActive(_ application: UIApplication) { }
+    func applicationWillTerminate(_ application: UIApplication) { }
+}
+
---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitcoinApplication
+//
+//  Created by Leonardo Modro on 30/01/2018.
+//  Copyright © 2018 Leonardo Modro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.black
+    }
+}
+
